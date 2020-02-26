bf3b5302450727733b2571a0f6d817d412db7f95
elizabethtrinitygs <elizabethtrinitygs@gmail.com>
2020-02-25 21:17:18 -0600

    creando una nueva ruta para que el enlace pueda redireccionar a la informacion, le debemos pasar un parametro (id) de lo que queremos mostrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { HomeComponent } from './components/home/home.component';
 import { AboutComponent } from './components/about/about.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
 
+import { HeroeComponent } from './components/heroe/heroe.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'heroes', component: HeroesComponent },
-  { path: '**', pathMatch: 'full', redirectTo: 'home' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' },
+// este path debe recibir un parametro, que sera el id del hero que vamos a mostrar
+  { path: 'heroe/:id', component: HeroeComponent}
 ];
 
 @NgModule({

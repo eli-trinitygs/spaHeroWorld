782d71a8cfeefe4c2f50a1424b0c825d54c212b9
elizabethtrinitygs <elizabethtrinitygs@gmail.com>
2020-03-04 00:31:01 -0600

    utilizando INPUT , permite llamar una variable desde afuera. Ahora, se ha hecho una  tarjeta que sera un componente reutilizable para mostrarla en otros componentes

--- src/app/components/heroe-card/heroe-card.component.ts
+++ src/app/components/heroe-card/heroe-card.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-heroe-card',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroe-card.component.css']
 })
 export class HeroeCardComponent implements OnInit {
+// HEROE es lo que se va a manejar en cada tarjeta
 
-  constructor() { }
+//se inicializa variable heroe como obj vacio
+// heroe: any = {};
+
+//Input = le dice a Angular que una prod que quiero specificar sera recibida desde afuera
+@Input() heroe: any = {};
+//la propiedad HEROE puede venir desde afuera
+
+@Input() index: number;
+
+  constructor(private router:Router) { }
 
   ngOnInit() {
   }
+//FUNCTION
+verHeroe() {
+  console.log(this.index);
 
+  this.router.navigate(['/heroe', this.index]);
+}
 }
